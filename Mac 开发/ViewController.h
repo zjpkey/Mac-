1f59d84dd7298c54c07c91b38c0730d552f57e9a
zjpkey <zjp@zjpdeMacBook-Air.local>
2016-04-26 09:33:47 +0800

    提交项目

--- Mac 开发/ViewController.h
+++ Mac 开发/ViewController.h
@@ -9,7 +9,24 @@
 #import <Cocoa/Cocoa.h>
 
 @interface ViewController : NSViewController
+@property (strong) IBOutlet NSTextField *textfilednum;
+@property (strong) IBOutlet NSTextField *t1;
 
+@property(assign) NSInteger type;
+@property (strong) IBOutlet NSTextField *t2;
+@property (strong) IBOutlet NSTextField *llable;
+@property (strong) IBOutlet NSButton *b0;
+@property (strong) IBOutlet NSButton *b1;
+@property (strong) IBOutlet NSButton *b2;
+@property (strong) IBOutlet NSButton *b3;
+@property (strong) IBOutlet NSButton *b4;
+@property (strong) IBOutlet NSButton *b5;
+@property (strong) IBOutlet NSButton *b6;
+@property (strong) IBOutlet NSButton *b7;
+@property (strong) IBOutlet NSButton *b8;
+@property (strong) IBOutlet NSButton *b9;
 
+@property(strong)NSString   *str1;
+@property(strong)NSString   *str2;
 @end
 
--- Mac 开发/ViewController.m
+++ Mac 开发/ViewController.m
@@ -21,5 +21,62 @@
 
     // Update the view, if already loaded.
 }
+- (IBAction)jisuan:(id)sender {
+    self.llable.stringValue = [NSString stringWithFormat:@"%ld",self.t1.integerValue + self.t2.integerValue];;
+}
+- (IBAction)chu:(NSButton *)sender {
+    self.type = 4;
+    self.str1 = self.textfilednum.stringValue;
+    [self clean];
+}
+- (IBAction)cheng:(NSButton *)sender {
+    self.type = 3;
+    self.str1 = self.textfilednum.stringValue;
+    [self clean];
+}
+- (IBAction)jian:(NSButton *)sender {
+    self.type = 2;
+    self.str1 = self.textfilednum.stringValue;
+    [self clean];
+}
+- (IBAction)jia:(NSButton *)sender {
+    self.type = 1;
+    self.str1 = self.textfilednum.stringValue;
+    [self clean];
+}
+- (IBAction)jisuanbut:(NSButton *)sender {
+    self.textfilednum.stringValue = [NSString stringWithFormat:@"%@%ld",self.textfilednum.stringValue,(long)sender.tag];
+    
+    self.str2 = self.textfilednum.stringValue;
+    
+}
+- (IBAction)jisuannum:(NSButton *)sender {
+    if (self.type == 1) {
+          self.textfilednum.stringValue = [NSString stringWithFormat:@"%ld",(long)(self.str1.integerValue + self.str2.integerValue)];;
+    }
+    if (self.type == 2) {
+            self.textfilednum.stringValue = [NSString stringWithFormat:@"%ld",(long)(self.str1.integerValue - self.str2.integerValue)];;
+    }
+    if (self.type == 3) {
+            self.textfilednum.stringValue = [NSString stringWithFormat:@"%ld",(long)(self.str1.integerValue * self.str2.integerValue)];;
+    }
+    if (self.type == 4) {
+        if (self.str2.integerValue == 0) {
+            self.textfilednum.stringValue = [NSString stringWithFormat:@"不是数字"];
+            return;
+        }
+            self.textfilednum.stringValue = [NSString stringWithFormat:@"%ld",(long)(self.str1.integerValue / self.str2.integerValue)];;
+    }
+
+}
+- (IBAction)qingchu:(NSButton *)sender {
+    self.str1 = @"";
+    self.str2 = @"";
+    self.textfilednum.stringValue = @"";
+}
+
+- (void)clean {
+    self.textfilednum.stringValue = @"";
+}
 
 @end
